4c5f167ef16b0cd3dccf97d05d7d13c56750024b
Yiqing Liang <liangyiqing.in.china@gmail.com>
2016-01-13 20:33:33 +0000

    finish

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -43,7 +43,7 @@ end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
-  s.length > 0 && s.match(/^[0|1]*$/) ?  s.to_i(2)%4 ==0 : false
+  s.length > 0 && (true if Float(string) rescue false) ?  s.to_i(2)%4 ==0 : false
 end
 
 # Part 3
